c23948cdf7db82e169861f521f5966a874fbbc99
John Helveston <john.helveston@gmail.com>
2021-04-08 09:00:27 -0400

    fixed site index page

--- build.R
+++ build.R
@@ -1,8 +1,8 @@
 # Render the slides
 xaringanBuilder::build_html(here::here("slides", "index.Rmd"))
 xaringanBuilder::build_pdf(
-    here::here("slides", "index.html"), "2021-sawtooth-conf-logitr-wtp.pdf")
+    here::here("slides", "index.html"),
+    here::here("slides", "2021-sawtooth-conf-logitr-wtp.pdf"))
 
-# Then render the README & index.html
-rmarkdown::render("README.Rmd", output_format = "github_document")
+# Then render the repo index page
 rmarkdown::render("index.Rmd")
